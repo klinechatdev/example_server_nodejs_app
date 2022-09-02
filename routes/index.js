0d0e949a3bdc8a23fb91454f6d01f7c23098b0c0
nla-asia <naylin.aung-3@kbzbank.com>
2022-09-03 01:33:49 +0630

    contacts crud api added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
-
-
-
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,6 +9,77 @@ router.get('/', function(req, res, next) {
 });
 
 
+/* GET contacts list. */
+router.get('/contacts', function(req, res, next) {
+  try {
+    let rawdata = fs.readFileSync('database/contacts.json');
+    let contacts = JSON.parse(rawdata);
+    res.json({success: true, message: "Retrieved successfully!", data: contacts});
+  } catch (error) {
+    res.json({success: false, message: "Retrieved error!", data: []});
+  }
+
+});
+
+
+/* POST contacts . */
+router.post('/contacts', function(req, res, next) {
+
+  try {
+    let data = req.body;
+    if(!data.id || !data.name|| !data.token){
+      return res.json({success: false, message: "Please provide all required fields!"});
+    }
+
+    let rawdata = fs.readFileSync('database/contacts.json');
+    let contacts = JSON.parse(rawdata);
+    let found = contacts.findIndex((v,i)=>v.id==data.id);
+    let newContact = {
+      ...data,
+      created_at : Date.now()
+    }
+    if(found>-1){ // if we found device, overwrite
+      contacts[found] = newContact
+    }else{
+      contacts.push(newContact);
+    }
+    let updateData = JSON.stringify(contacts);
+    fs.writeFileSync('database/contacts.json', updateData);
+
+    return res.json({success: true, message: "Contact Created successfully!"});
+  } catch (error) {
+    console.log(error);
+    return res.json({success: false, message: "Contact creation error!"});
+  }
+
+});
+
+/* DELETE contacts . */
+router.delete('/contacts', function(req, res, next) {
+
+  try {
+    let id = req.body.id;
+    if(!id){
+      return res.json({success: false, message: "You must provide id to delete a contact"});
+    }
+
+    let rawdata = fs.readFileSync('database/contacts.json');
+    let contacts = JSON.parse(rawdata);
+    let found = contacts.findIndex((v,i)=>v.id==id);
+
+    if(found>-1){
+        contacts.splice(found, 1);
+        let updateData = JSON.stringify(contacts);
+        fs.writeFileSync('database/contacts.json', updateData);
+    }
+    
+    res.json({success: true, message: "Contact deleted successfully!"});
+  } catch (error) {
+    console.log(error);
+    res.json({success: false, message: "Contact deletion error!"});
+  }
+
+});
 
 
 module.exports = router;
